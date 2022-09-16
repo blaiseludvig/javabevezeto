35e86b58f5ae6552f08b697f210c8ad4ec11a6e7
blaiseludvig <blaiseludvig@gmail.com>
2022-09-17 01:59:10 +0200

    add formatting to task 9

--- src/hu/petrik/javabevezeto/Feladat9.java
+++ src/hu/petrik/javabevezeto/Feladat9.java
@@ -3,11 +3,24 @@ package hu.petrik.javabevezeto;
 public class Feladat9 {
 
     public static void run() {
+        System.out.println("Az összes hárommal és öttel egyaránt osztható, 1000-nél kissebb természetes szám: ");
+
+        int j = 0;
         for (int i = 0; i < 1000; i++) {
             if (i % 5 == 0 && i % 3 == 0) {
                 System.out.printf("%d, ", i);
+                j += 1;
             }
+
+            if (j == 5){
+                System.out.print('\n');
+                j = 0;
+            }
+
         }
+
+        System.out.print('\n');
+
     }
 
 }
