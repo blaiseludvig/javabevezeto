fc5b6d0c6a292fb557ae455d198e5b4cd05df8ad
blaiseludvig <blaiseludvig@gmail.com>
2022-09-17 02:34:15 +0200

    fix task 6 being a duplicate of task 7

--- src/hu/petrik/javabevezeto/Feladat6.java
+++ src/hu/petrik/javabevezeto/Feladat6.java
@@ -1,37 +1,30 @@
 package hu.petrik.javabevezeto;
 
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class Feladat6 {
 
     static Scanner in = new Scanner(System.in);
 
-    public static void run() {
-        System.out.print("Hányadik hónapot írunk: ");
-        int n = Integer.parseInt(in.nextLine());
+    static final double[] grade_boundaries = {43, 58, 73, 88, 100};
+    static final String[] grade_strings = {"elégtelen", "elégséges", "közepes", "jó", "jeles"};
 
-        System.out.printf("Éppen %s van.%n", evszak(n));
+    public static void run() {
+        System.out.print("Adja meg a dolgozat pontszámát: ");
+        double score = Double.parseDouble(in.nextLine());
+
+        System.out.printf("A dolgozat %s.%n", getGrade(score));
 
     }
 
-    static String evszak(int n) {
-        HashMap<Integer, String> evszakok = new HashMap<>();
-        evszakok.put(1, "tél");
-        evszakok.put(2, "tél");
-        evszakok.put(3, "tavasz");
-        evszakok.put(4, "tavasz");
-        evszakok.put(5, "tavasz");
-        evszakok.put(6, "nyár");
-        evszakok.put(7, "nyár");
-        evszakok.put(8, "nyár");
-        evszakok.put(9, "ősz");
-        evszakok.put(10, "ősz");
-        evszakok.put(11, "ősz");
-        evszakok.put(12, "tél");
+    static String getGrade(double score) {
+        for (int i = 0; i < grade_boundaries.length; i++) {
+            if (score < grade_boundaries[i]) {
+                return grade_strings[i];
+            }
+        }
 
-        return evszakok.get(n);
-
+        return grade_strings[grade_strings.length - 1];
     }
 
 }
